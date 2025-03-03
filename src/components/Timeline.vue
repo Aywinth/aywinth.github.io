3ec49e58ae94ff5685ad2264b663a8107036d38a
Aywinth <samuel.calderoni@gmail.com>
2025-03-03 15:20:44 +0100

    Update of the timeline with real information

---
+++ src/components/Timeline.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="container py-5">
+        <div class="main-timeline-2">
+
+            <!-- TODO: Add my name + picture on top of the page, on another component
+                 TODO: Add reactivity to the cards, to go on a page with more information
+                 TODO: Add a mean of contact at the bottom like LinkedIn or Mail -->
+
+            <!-- PhD -->
+            <div class="timeline-2 left-2">
+                <div class="card">
+                    <img src="/stille.svg" class="card-img-top timeling-img stille" alt="Responsive image">
+                    <div class="card-body p-4">
+                        <h4 class="fw-bold">PhD Student - CEISAM</h4>
+                        <p class="text-muted mb-4"><i class="far fa-clock" aria-hidden="true"></i>Nov. 2023 - Now</p>
+                        <p class="mb-0">The synthesis of high-value molecules remains a primary objective in organic
+                            chemistry.
+                            Despite the vast diversity of available reactions, the continuous development of novel
+                            transformations is essential to facilitate the synthesis of complex, often highly
+                            functionalized,
+                            molecules. Historically, transition metal-catalyzed coupling reactions were considered
+                            effective
+                            primarily for coupling sp or sp²-hybridized carbon atoms. However, recent advancements have
+                            extended their applicability to sp³-hybridized carbons.
+                            With these insights, developing a pallado-catalyzed formylation reaction seems worthwhile as
+                            it tackles several key drawbacks of current approaches.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Dev -->
+            <div class="timeline-2 right-2">
+                <div class="card">
+                    <img src="/dev.svg" class="card-img-top timeline-img dev" alt="Responsive image">
+                    <div class="card-body p-4">
+                        <h4 class="fw-bold">Python & Web Development - Udemy</h4>
+                        <p class="text-muted mb-4"><i class="far fa-clock" aria-hidden="true"></i>Sep. 2022 - Now</p>
+                        <p class="mb-0">Skills diversification and strengthening through training in Python
+                            <span class="examples">(OOP, data scraping, statistical analysis, machine learning)</span>
+                            and full-stack web development
+                            <span class="examples">(HTML, CSS, JavaScript, Vue, Express, Node, PostgreSQL,
+                                etc...)</span>.
+                            Certifications have been obtained, ensuring proficiency in building scalable applications."
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Diazocell -->
+            <div class="timeline-2 left-2">
+                <div class="card">
+                    <img src="/diazocell.svg" class="card-img-top timeline-img diazocell" alt="Responsive image">
+                    <div class="card-body p-4">
+                        <h4 class="fw-bold">R&D Engineer - SATT Ouest-Valorisation</h4>
+                        <p class="text-muted mb-4"><i class="far fa-clock" aria-hidden="true"></i>Nov. 2020 - Jun. 2022
+                        </p>
+                        <p class="mb-0">The project aims to develop a new ultra-fast and spatially controlled method to
+                            functionalize cellulose through the creation of a covalent linkage. In this specific case,
+                            hydrophobic compounds are synthesized and grafted onto cellulose via our method, to obtain
+                            hydrophobic cellulose as new materials.</p>
+                        <div class="litterature">
+                            <span>S. Calderoni
+                                <span style="font-style: italic;">et al. ACS Appl. Polym. Mater. </span>
+                                <span style="font-weight: bold;">2024</span>,
+                                <span style="font-style: italic;">6</span>,
+                                13, 7772–7780.
+                            </span>
+                            <span>WO2021233959</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Sanofi -->
+            <div class="timeline-2 right-2">
+                <div class="card">
+                    <img src="/qsar-studies.svg" class="card-img-top timeline-img qsar" alt="Responsive image">
+                    <div class="card-body p-4">
+                        <h4 class="fw-bold">Master 2 Internship - Sanofi</h4>
+                        <p class="text-muted mb-4"><i class="far fa-clock" aria-hidden="true"></i> Feb. 2020 - Aug. 2020
+                        </p>
+                        <p class="mb-0">The project aims to synthesize novel series of original compounds and realize
+                            QSAR studies on these different series.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Kidamycin -->
+            <div class="timeline-2 left-2">
+                <div class="card">
+                    <img src="/kidamycin.svg" class="card-img-top timeline-img kidamycin" alt="kidamycin structure">
+                    <div class="card-body p-4">
+                        <h4 class="fw-bold">Master 1 Internship - CEISAM</h4>
+                        <p class="text-muted mb-4"><i class="far fa-clock" aria-hidden="true"></i>Mar. 2019 - Jun. 2019
+                        </p>
+                        <p class="mb-0">The project aims to achieve the first total synthesis of kidamycin. A new
+                            convergent synthesis strategy was developped to afford kidamycin, and more generally
+                            pluramycins, a class of antibiotics to which kidamycin belongs.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
